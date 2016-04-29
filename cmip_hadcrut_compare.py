858d99c5ad9cdb2828dfa3afff304a0147082f76
hansbrenna <hans.brenna@geo.uio.no>
2016-04-29 19:30:59 +0200

    finished methinks

--- cmip_hadcrut_compare.py
+++ cmip_hadcrut_compare.py
@@ -9,31 +9,52 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib
+matplotlib.use('PS')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-CMIP=pd.read_excel('C:/Users/hanbre/Documents/CMIP5_Models_Air_Temp_One_Member.xlsx')
-CMIP = CMIP[:2005]-273.15
+sns.set(font_scale=1.5)
+
+CMIP=pd.read_excel('CMIP5_Models_Air_Temp_One_Member.xlsx')
+CMIP = CMIP-273.15
 CMIP = pd.rolling_mean(CMIP,60)
 
-hadcrut = pd.read_excel('C:/Users/hanbre/Documents/hadcrut_temps.xlsx')
+hadcrut = pd.read_excel('hadcrut_temps.xlsx')
 hadcrut = pd.rolling_mean(hadcrut,60)
 
 had_20m = hadcrut.loc[1900:2000].mean()
 
-hada = hadcrut[:2005]-had_20m
+hada = hadcrut-had_20m
 
 CMIPa = CMIP-had_20m['Global']
 
 Cmean = CMIPa.mean(axis='columns')
 Cstd = CMIPa.std(axis='columns')
 
-plt.figure(figsize=(10,4))
+fig=plt.figure(figsize=(10,4))
 plt.hold(True)
 
-plt.plot(Cmean,linewidth=3,label='CMIP5 mean')
-plt.plot(Cmean+Cstd,color='0.5',linewidth=2,label='CMIP5 std')
-plt.plot(Cmean-Cstd,color='0.5',linewidth=2)
-plt.plot(hada['Global'],linewidth=3,label='HadCRUT4')
-plt.legend(fontsize=16)
-plt.title('5 year rolling mean CMIP5 and HadCRUT4 global temperatures',fontsize=18)
+plt.plot(Cmean[:2005],linewidth=3,label='CMIP5 mean')
+plt.plot(Cmean[:2005]+Cstd[:2005],color='0.5',linewidth=2,label='CMIP5 std')
+plt.plot(Cmean[:2005]-Cstd[:2005],color='0.5',linewidth=2)
+plt.plot(hada['Global'].loc[:2005],linewidth=3,label='HadCRUT4')
+plt.legend(fontsize=14,loc='upper left')
+plt.title('5 year rolling mean CMIP5 and HadCRUT4 global temperatures',fontsize=16)
+plt.xlabel('Year',fontsize='14')
+plt.ylabel('Temperature ralative to HadCRUT4 1900-2000')
+
+fig.savefig('hadcrut_cmip_comparison.png',bbox_inches='tight')
+
+fig2=plt.figure(figsize=(10,4))
+plt.hold(True)
+
+plt.plot(Cmean[2005:2016],linewidth=3,label='CMIP5 mean')
+plt.plot(Cmean[2005:2016]+Cstd[2005:2016],color='0.5',linewidth=2,label='CMIP5 std')
+plt.plot(Cmean[2005:2016]-Cstd[2005:2016],color='0.5',linewidth=2)
+plt.plot(hada['Global'].loc[2005:2016],linewidth=3,label='HadCRUT4')
+plt.legend(fontsize=14,loc='upper left')
+plt.title('5 year rolling mean CMIP5 and HadCRUT4 global temperatures',fontsize=16)
+plt.xlabel('Year',fontsize='14')
+plt.ylabel('Temperature ralative to HadCRUT4 1900-2000')
+
+fig2.savefig('hadcrut_cmip_comparison2005-2016.png',bbox_inches='tight')
